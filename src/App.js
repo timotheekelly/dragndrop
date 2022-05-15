560a0a635030b855aceb137152608a1aed4d62c9
Tim Kelly <tkelly3118@gmail.com>
2022-05-15 15:15:57 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="drag-n-drop">
+          <div className="dnd-group">
+            <div className="dnd-item">
+              <div>
+                <p>ITEM</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
